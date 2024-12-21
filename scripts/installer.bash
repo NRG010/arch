33ad5a3def26ee3b4c4eb94e98ac369995e30e1a
Baldev Singh <personalemailofbaldev@gmail.com>
2024-12-21 13:02:45 +0530

    21/12/24 12:12

--- scripts/installer.bash
+++ scripts/installer.bash
@@ -4,22 +4,8 @@ sudo pacman -Syu
 # A.U.R.
 sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay.git
-cd yay
+cd yay || exit
 makepkg -si
-
-# Graphic card
-yay -S --needed linux-headers nvidia neovim
-sudo cp ./grub /etc/default/grub
-sudo nvim /etc/default/grub
-sudo cp ./mkinitcpio.conf /etc/mkinitcpio.conf
-sudo nvim /etc/mkinitcpio.conf
-sudo mkdir -p /etc/modprobe.d/
-sudo cp ./nvidia.conf /etc/modprobe.d/
-sudo nvim /etc/modprobe.d/nvidia.conf
-sudo mkdir -p /etc/pacman.d/
-sudo cp ./nvidia.hook /etc/pacman.d/hooks/
-sudo grub-mkconfig -o /boot/grub/grub.cfg
-sudo mkinitcpio -P
 
 # Fonts
 yay -S --needed noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-cascadia-code-nerd
@@ -34,14 +20,11 @@ yay -S --needed slurp grim swappy
 # Window manager
 yay -S --needed hyprland hyprpaper wofi dunst bpytop
 
-# File explorer
-yay -S --needed imv zathura zathura-cb zathura-pdf-mupdf
-
-# Media
-yay -S --needed qutebrowser mpv yt-dlp obsidian qbittorrent
-
 # Network
 yay -S --needed nm-connection-editor networkmanager-openvpn
+
+# Media
+yay -S --needed qutebrowser mpv yt-dlp qbittorrent steam python-adblock
 
 # A.U.R applications
 yay -S --needed bibata-cursor-theme vivaldi-widevine wofi-emoji ani-cli uwsm wezterm-git
@@ -52,11 +35,14 @@ yay -S --needed qt5ct qt5-wayland qt6ct qt6-wayland kvantum kvantum-qt5 gtk3 nwg
 # Audio
 yay -S --needed pipewire pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack bluez bluez-utils pamixer
 
-# Utilities
-yay -S --needed xdg-desktop-portal-hyprland xdg-desktop-portal-gtk wlsunset wl-clipboard cliphist brightnessctl polkit-kde-agent polkit unzip
+# File explorer
+yay -S --needed imv zathura zathura-cb zathura-pdf-mupdf yazi ffmpeg p7zip jq poppler fd ripgrep fzf zoxide imagemagick
 
 # Coding
-yay -S --needed pyright python-black python-isort shfmt bash-language-server stylua lua-language-server clang python-adblock rustup rust-analyzer github-cli
+yay -S --needed pyright python-black python-isort shfmt bash-language-server stylua lua-language-server clang rustup rust-analyzer
+
+# Utilities
+yay -S --needed xdg-desktop-portal-hyprland xdg-desktop-portal-gtk wlsunset wl-clipboard cliphist brightnessctl polkit-kde-agent github-cli
 
 # Cleaning
 yay -Yc
