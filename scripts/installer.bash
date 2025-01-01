81ebf26992689ea73fd77fb0c55d5a33b41acf0d
Baldev Singh <personalemailofbaldev@gmail.com>
2025-01-01 11:48:31 +0530

    01/01/25 11:01

--- scripts/installer.bash
+++ scripts/installer.bash
@@ -13,11 +13,11 @@ yay -S --needed slurp grim swappy
 # Network
 yay -S --needed nm-connection-editor networkmanager-openvpn
 
-# Window manager
-yay -S --needed hyprland hyprpaper wofi dunst bpytop alacritty
-
 # Media
 yay -S --needed qutebrowser mpv yt-dlp qbittorrent python-adblock
+
+# Window manager
+yay -S --needed hyprland hyprpaper wofi dunst bpytop alacritty ueberzugpp
 
 # A.U.R applications
 yay -S --needed bibata-cursor-theme vivaldi-widevine wofi-emoji ani-cli uwsm
@@ -28,14 +28,14 @@ yay -S --needed qt5ct qt5-wayland qt6ct qt6-wayland kvantum kvantum-qt5 gtk3 nwg
 # Audio
 yay -S --needed pipewire pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack bluez bluez-utils pamixer
 
-# Cli
-yay -S --needed imv zathura zathura-cb zathura-pdf-mupdf yazi ffmpeg p7zip jq poppler fd ripgrep fzf zoxide imagemagick bat lsd wget github-cli
-
 # Coding
 yay -S --needed pyright python-black python-isort shfmt bash-language-server stylua lua-language-server clang rust-analyzer
 
 # Utilities
 yay -S --needed xdg-desktop-portal-hyprland xdg-desktop-portal-gtk wlsunset wl-clipboard cliphist brightnessctl polkit-kde-agent
+
+# Cli
+yay -S --needed imv zathura zathura-cb zathura-pdf-mupdf yazi ffmpeg p7zip jq poppler fd ripgrep fzf zoxide imagemagick bat lsd wget github-cli
 
 # Misc.
 sudo systemctl enable bluetooth.service
