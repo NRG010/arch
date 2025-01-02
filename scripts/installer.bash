6d07e2139571dc318a9d938c3e9208639a1a9846
Baldev Singh <personalemailofbaldev@gmail.com>
2025-01-02 10:37:09 +0530

    02/01/25 10:01

--- scripts/installer.bash
+++ scripts/installer.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Fonts
-yay -S --needed noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-cascadia-code-nerd
+yay -S --needed noto-fonts{,-{cjk,emoji,extra}} ttf-cascadia-code-nerd
 
 # Fish
 yay -S --needed fish starship
@@ -14,28 +14,43 @@ yay -S --needed slurp grim swappy
 yay -S --needed nm-connection-editor networkmanager-openvpn
 
 # Media
-yay -S --needed qutebrowser mpv yt-dlp qbittorrent python-adblock
+yay -S --needed qutebrowser mpv yt-dlp qbittorrent imv
 
 # Window manager
-yay -S --needed hyprland hyprpaper wofi dunst bpytop alacritty ueberzugpp
+yay -S --needed hypr{land,paper} wofi dunst bpytop ghostty polkit-kde-agent
 
 # A.U.R applications
 yay -S --needed bibata-cursor-theme vivaldi-widevine wofi-emoji ani-cli uwsm
 
 # Theme
-yay -S --needed qt5ct qt5-wayland qt6ct qt6-wayland kvantum kvantum-qt5 gtk3 nwg-look nwg-displays
+yay -S --needed qt{5,6}ct qt{5,6}-wayland kvantum{,-qt5} gtk{3,4} nwg-{look,displays}
 
 # Audio
-yay -S --needed pipewire pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack bluez bluez-utils pamixer
+yay -S --needed pipewire{,-{audio,alsa,pulse,jack}} pamixer
 
-# Coding
-yay -S --needed pyright python-black python-isort shfmt bash-language-server stylua lua-language-server clang rust-analyzer
+# Bluetooth
+yay -S --needed bluez{,-utils}
 
-# Utilities
-yay -S --needed xdg-desktop-portal-hyprland xdg-desktop-portal-gtk wlsunset wl-clipboard cliphist brightnessctl polkit-kde-agent
+# Python
+yay -S --needed tk python-{black,isort,numpy,pygame,matplotlib,adblock}
+
+# Lsp
+yay -S --needed clang {lua,bash}-language-server rust-analyzer pyright
+
+# Format
+yay -S --needed shfmt stylua
+
+# Screen
+yay -S --needed xdg-desktop-portal-{hyprland,gtk} wlsunset brightnessctl
+
+# Clipboard
+yay -S --needed wl-clipboard cliphist
+
+# Documents
+yay -S --needed zathura{,-{cb,pdf-mupdf}}
 
 # Cli
-yay -S --needed imv zathura zathura-cb zathura-pdf-mupdf yazi ffmpeg p7zip jq poppler fd ripgrep fzf zoxide imagemagick bat lsd wget github-cli
+yay -S --needed yazi ffmpeg p7zip jq poppler fd ripgrep fzf zoxide imagemagick bat lsd wget github-cli
 
 # Misc.
 sudo systemctl enable bluetooth.service
