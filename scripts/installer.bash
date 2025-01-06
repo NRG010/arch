18a50179f9c520a1cdc0a664ee2685a4293fcb45
Baldev Singh <personalemailofbaldev@gmail.com>
2025-01-06 14:15:32 +0530

    06/01/25 14:01

--- scripts/installer.bash
+++ scripts/installer.bash
@@ -20,7 +20,7 @@ paru -S --needed shfmt stylua yapf
 paru -S --needed wl-clipboard cliphist
 
 # Documents
-paru -S --needed zathura{,-{cb,pdf-mupdf}}
+paru -S --needed zathura{,-{cb,pdf-poppler,djvu,ps}}
 
 # Gaming
 paru -S --needed steam wine{,-{gecko,mono}}
@@ -31,20 +31,20 @@ paru -S --needed tk python-{lsp-server,adblock}
 # Screen
 paru -S --needed xdg-desktop-portal-{hyprland,gtk}
 
-# A.U.R applications
-paru -S --needed bibata-cursor-theme wofi-emoji ani-cli uwsm
-
 # Network
 paru -S --needed nm-connection-editor networkmanager-openvpn
+
+# A.U.R applications
+paru -S --needed bibata-cursor-theme wofi-emoji ani-cli uwsm
 
 # Lsp
 paru -S --needed {lua,bash}-language-server rust-analyzer clang
 
+# Theme
+paru -S --needed qt{5,6}{ct,-wayland} kvantum{,-qt5} gtk{3,4} nwg-look
+
 # Audio
 paru -S --needed pipewire{,-{audio,alsa,pulse,jack}} pamixer pavucontrol
-
-# Theme
-paru -S --needed qt{5,6}ct qt{5,6}-wayland kvantum{,-qt5} gtk{3,4} nwg-look
 
 # Window manager
 paru -S --needed hypr{land,paper} wofi dunst bpytop ghostty polkit-kde-agent libnotify
