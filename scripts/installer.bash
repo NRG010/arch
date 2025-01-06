04c9bb90486ac17d7f393b6a0bf1a1b5f783e747
Baldev Singh <personalemailofbaldev@gmail.com>
2025-01-06 10:53:11 +0530

    06/01/25 10:01

--- scripts/installer.bash
+++ scripts/installer.bash
@@ -7,44 +7,14 @@ paru -S --needed noto-fonts{,-{cjk,emoji,extra}} ttf-cascadia-code-nerd
 paru -S --needed fish starship
 chsh -s /usr/bin/fish
 
-# Screenshot
-paru -S --needed slurp grim swappy
-
-# Network
-paru -S --needed nm-connection-editor networkmanager-openvpn
-
-# Media
-paru -S --needed qutebrowser mpv yt-dlp qbittorrent imv
-
-# Window manager
-paru -S --needed hypr{land,paper} wofi dunst bpytop ghostty polkit-kde-agent
-
-# A.U.R applications
-paru -S --needed bibata-cursor-theme vivaldi-widevine wofi-emoji ani-cli uwsm
-
-# Theme
-paru -S --needed qt{5,6}ct qt{5,6}-wayland kvantum{,-qt5} gtk{3,4} nwg-look
-
-# Audio
-paru -S --needed pipewire{,-{audio,alsa,pulse,jack}} pamixer
-
-# C/C++
-paru -S --needed clang
-
 # Bluetooth
 paru -S --needed bluez{,-utils}
 
+# Screenshot
+paru -S --needed slurp grim swappy
+
 # Format
 paru -S --needed shfmt stylua yapf
-
-# Python
-paru -S --needed tk python-{lsp-server,adblock}
-
-# Lsp
-paru -S --needed {lua,bash}-language-server rust-analyzer
-
-# Screen
-paru -S --needed xdg-desktop-portal-{hyprland,gtk}
 
 # Clipboard
 paru -S --needed wl-clipboard cliphist
@@ -52,11 +22,41 @@ paru -S --needed wl-clipboard cliphist
 # Documents
 paru -S --needed zathura{,-{cb,pdf-mupdf}}
 
-# Cli
-paru -S --needed yazi ffmpeg p7zip jq poppler fd ripgrep fzf zoxide imagemagick bat lsd wget github-cli
-
 # Gaming
 paru -S --needed steam wine{,-{gecko,mono}}
+
+# Python
+paru -S --needed tk python-{lsp-server,adblock}
+
+# Screen
+paru -S --needed xdg-desktop-portal-{hyprland,gtk}
+
+# Media
+paru -S --needed qutebrowser mpv yt-dlp qbittorrent imv
+
+# Browser
+paru -S --needed vivaldi vivaldi-ffmpeg-codecs libnotify
+
+# A.U.R applications
+paru -S --needed bibata-cursor-theme wofi-emoji ani-cli uwsm
+
+# Network
+paru -S --needed nm-connection-editor networkmanager-openvpn
+
+# Lsp
+paru -S --needed {lua,bash}-language-server rust-analyzer clang
+
+# Audio
+paru -S --needed pipewire{,-{audio,alsa,pulse,jack}} pamixer pavucontrol
+
+# Theme
+paru -S --needed qt{5,6}ct qt{5,6}-wayland kvantum{,-qt5} gtk{3,4} nwg-look
+
+# Window manager
+paru -S --needed hypr{land,paper} wofi dunst bpytop ghostty polkit-kde-agent
+
+# Cli
+paru -S --needed yazi ffmpeg p7zip jq poppler fd ripgrep fzf zoxide imagemagick bat lsd wget github-cli
 
 # Misc.
 sudo systemctl enable bluetooth.service
